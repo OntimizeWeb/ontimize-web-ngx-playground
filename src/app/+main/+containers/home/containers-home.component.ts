9abdc0d37b676bb7d5794dbfd4124f5ab2a32c3d
daniel-grana <daniel.grana@imatia.com>
2016-11-23 19:52:20 +0100

    updating code

--- src/app/+main/+containers/home/containers-home.component.ts
+++ src/app/+main/+containers/home/containers-home.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 
+import { MdRadioGroup } from '@angular/material';
+
 
 @Component({
   selector: 'containers-home',
@@ -9,9 +11,49 @@ import { Component, ViewChild } from '@angular/core';
 })
 export class ContainersHomeComponent {
 
+  protected layoutHorizontal: string = "start";
+  protected layoutVertical: string = "stretch";
+
+  protected dataHorizontal: Array<Object> = [];
+  protected dataVertical: Array<Object> = [];
 
   constructor() {
   }
 
+  ngOnInit() {
+    this.dataHorizontal = [
+      { "value": "none", "label": "none", "checked": false },
+      { "value": "start", "label": "start (default)", "checked": true },
+      { "value": "center", "label": "center", "checked": false },
+      { "value": "end", "label": "end", "checked": false },
+      { "value": "space-around", "label": "space-around", "checked": false },
+      {"value": "space-between", "label": "space-between", "checked": false}
+    ];
+    this.dataVertical = [
+      { "value": "none", "label": "none", "checked": false },
+      { "value": "start", "label": "start", "checked": false },
+      { "value": "center", "label": "center", "checked": false },
+      { "value": "end", "label": "end", "checked": false },
+      { "value": "stretch", "label": "stretch (default)", "checked": true }
+    ];
+  }
+
+  getLayoutAlign() {
+    return this.layoutHorizontal + " " + this.layoutVertical;
+  }
+
+  onLayoutHorizontalChange(evt, layHor: MdRadioGroup) {
+    var self = this;
+    window.setTimeout(() => {
+      self.layoutHorizontal = evt.value;
+    }, 0);
+  }
+
+   onLayoutVerticalChange(evt, layHor: MdRadioGroup) {
+    var self = this;
+    window.setTimeout(() => {
+      self.layoutVertical = evt.value;
+    }, 0);
+  }
 
 }
