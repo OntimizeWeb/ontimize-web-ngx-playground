b313ea5f4269eaf90f505c7f0e32321a5f07ac89
Patricia Martinez <patricia.martinez@imatia.com>
2017-11-28 08:26:46 +0100

    Updating dialogs examples

--- src/app/main/dialogs/dialogs.component.ts
+++ src/app/main/dialogs/dialogs.component.ts
@@ -7,14 +7,76 @@ import {
 import { NavigationBarService } from '../../shared/navigation-bar.service';;
 
 const HTML_DATA = `
-<o-button attr="flat" type="FLAT" label="FLAT" layout-padding></o-button>
-
-<o-button attr="raised" type="RAISED" label="RAISED" layout-padding></o-button>
-
-<o-button attr="floating" type="FLOATING" icon="add" layout-padding></o-button>
+  <o-button attr="alert" type="RAISED" label="ALERT_DIALOG" layout-padding (click)="showAlert($event)" ></o-button>
+  <o-button attr="info" type="RAISED" label="INFO_DIALOG" layout-padding (click)="showInfo($event)" ></o-button>
+  <o-button attr="warn" type="RAISED" label="WARNING_DIALOG" layout-padding (click)="showWarning($event)" ></o-button>
+  <o-button attr="error" type="RAISED" label="ERROR_DIALOG" layout-padding (click)="showError($event)" ></o-button>
+  <o-button attr="confirm" type="RAISED" label="CONFIRM_DIALOG" layout-padding (click)="showConfirm($event)"></o-button>
 `;
 
-const TYPESCRIPT_DATA = ``;
+const TYPESCRIPT_DATA = `
+import {
+  DialogService,
+  ODialogConfig
+} from 'ontimize-web-ngx';
+
+@Component({
+  selector: 'app-dialogs',
+  templateUrl: './dialogs.component.html',
+  styleUrls: ['./dialogs.component.scss']
+})
+
+export class DialogsComponent implements OnInit {
+    constructor(
+      protected dialogService: DialogService) {
+    }
+
+    showAlert(evt: any) {
+      if (this.dialogService) {
+        this.dialogService.alert('Alert dialog title', 'This is an amazing "Alert" dialog');
+      }
+    }
+
+    showInfo(evt: any) {
+      if (this.dialogService) {
+        const config: ODialogConfig = {
+          icon: 'alarm',
+          okButtonText: 'It rocks!'
+        };
+        this.dialogService.info('Info dialog title',
+          'This is an amazing "Information" dialog', config);
+      }
+    }
+
+
+    showWarning(evt: any) {
+      if (this.dialogService) {
+        const config: ODialogConfig = {
+          icon: 'alarm',
+          okButtonText: 'It rocks!'
+        };
+        this.dialogService.warn('Warning dialog title', 'This is an amazing "Warning" dialog',
+          config);
+      }
+    }
+
+    showError(evt: any) {
+      if (this.dialogService) {
+        const config: ODialogConfig = {
+          icon: 'alarm',
+          okButtonText: 'It rocks!'
+        };
+        this.dialogService.error('Error dialog title', 'This is an amazing "Error" dialog', config);
+      }
+    }
+
+    showConfirm(evt: any) {
+      if (this.dialogService) {
+        this.dialogService.confirm('Confirm dialog title', 'Do you really want to accept?');
+      }
+    }
+  }
+`;
 
 @Component({
   selector: 'app-dialogs',
