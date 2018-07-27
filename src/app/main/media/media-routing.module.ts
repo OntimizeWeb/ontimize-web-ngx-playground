962c0830c3ad5fde4922d3f117b92d83e837f773
manu-cr <manuelcidrodriguez@gmail.com>
2018-07-27 16:42:47 +0200

    Fix compilation error

--- src/app/main/media/media-routing.module.ts
+++ src/app/main/media/media-routing.module.ts
@@ -1,22 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ImageComponent } from 'app/main/media/image-media/image.component';
+
 import { MediaComponent } from './media.component';
+import { ImageComponent } from './image-media/image.component';
 import { ImageExamplesComponent } from './image-examples/image-examples.component';
-
-
 
 export const routes: Routes = [
   {
     path: '', component: MediaComponent,
     children: [
       { path: '', redirectTo: 'playground', pathMatch: 'prefix' },
-      { path: 'playground', component: ImageComponent},
+      { path: 'playground', component: ImageComponent },
       { path: 'examples', component: ImageExamplesComponent }
     ]
   }
 ];
-
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
