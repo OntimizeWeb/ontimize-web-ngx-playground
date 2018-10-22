d10ebf841b4351f87e87803991a56173f6fd73a4
Patricia Martinez <patricia.martinez@imatia.com>
2018-10-22 11:53:31 +0200

    Import hammerjs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'hammerjs';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -14,3 +15,4 @@ const promise = platformBrowserDynamic().bootstrapModule(AppModule);
 promise.then(ontimizePostBootstrap).catch(err => {
   console.error(err.message);
 });
+
