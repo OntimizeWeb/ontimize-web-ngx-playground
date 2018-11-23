054f0c330224f88dc880d6595092f390967b0efc
Patricia Martinez <patricia.martinez@imatia.com>
2018-11-23 08:25:00 +0100

    Updated the example of o-slider

--- src/app/main/inputs/20.slider/input-slider.component.ts
+++ src/app/main/inputs/20.slider/input-slider.component.ts
@@ -19,17 +19,17 @@ const SLIDE_HTML_DATA = `
   <o-form editable-detail="false" show-header="no" layout-direction="row">
     <div fxLayout="column" layout-padding fxFlex="30">
       <label class="input-comp-title">{{ 'INPUTS.READ_ONLY' | oTranslate }}</label>
-      <o-slider attr="slider" [data]="getValue()" [vertical]="true" color="warn"></o-slider>
+      <o-slider attr="slider" [data]="getValue()" layout="column" color="warn"></o-slider>
     </div>
     <div fxLayout="column" layout-padding fxFlex="30">
       <label class="input-comp-title">{{ 'INPUTS.EDITABLE' | oTranslate }}</label>
       <o-slider attr="slider-editable" [data]="getValue()" read-only="no"
-        tooltip="This is an awesome tooltip!" vertical="true" color="warn" max="100" min="0" thumb-label="true"
+        tooltip="This is an awesome tooltip!" layout="column" color="warn" max="100" min="0" thumb-label="true"
         tick-interval="auto"></o-slider>
     </div>
     <div fxLayout="column" layout-padding fxFlex="30">
       <label class="input-comp-title">{{ 'INPUTS.DISABLED' | oTranslate }}</label>
-      <o-slider attr="slider-disabled" enabled="no" [vertical]="true" color="warn"
+      <o-slider attr="slider-disabled" enabled="no" layout="column" color="warn"
         [data]="getValue()"></o-slider>
     </div>
   </o-form>
