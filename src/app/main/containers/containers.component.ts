513cfecb8b07a997d0f36e0b10de64e150bbc5ca
Manuel Cid <manuelcidrodriguez@gmail.com>
2019-01-10 12:04:23 +0100

    Update container example

--- src/app/main/containers/containers.component.ts
+++ src/app/main/containers/containers.component.ts
@@ -1,24 +1,15 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-// import { MdRadioGroup } from '@angular/material';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 
-import { NavigationBarService } from '../../shared/navigation-bar.service';;
+import { ExampleComponent } from '../../shared/example/example.component';
+import { NavigationBarService } from '../../shared/navigation-bar.service';
 
-const HTML_ROW_DATA = `
-<o-row attr="row1" title="Row title" layout-align="start stretch"
-  elevation="2">
-  <div layout-padding style="background-color:#3498db;" > One </div>
-  <div layout-padding style="background-color:#e74c3c;" > Two </div>
-  <div layout-padding style="background-color:#f9c922;"> Three </div>
-</o-row>
-`;
 
-const HTML_COL_DATA = `
-<o-column attr="column1" title="Column title" layout-align="start stretch"
-  elevation="2">
-  <div layout-padding style="background-color:#3498db;" > One </div>
-  <div layout-padding style="background-color:#e74c3c;" > Two </div>
-  <div layout-padding style="background-color:#f9c922;"> Three </div>
-</o-column>
+const HTML_DATA = `
+<{layout} attr="container" title="{title}" icon="{icon}" layout-align="{layoutAlign}" elevation="{elevation}">
+  <div layout-padding style="background-color:#3498db;">One</div>
+  <div layout-padding style="background-color:#e74c3c;">Two</div>
+  <div layout-padding style="background-color:#f9c922;">Three</div>
+</{layout}>
 `;
 
 const TYPESCRIPT_DATA = ``;
@@ -34,12 +25,20 @@ const TYPESCRIPT_DATA = ``;
 })
 export class ContainersComponent implements OnInit {
 
-  layout = 'row';
+  public layout: string = 'row';
   protected layoutHorizontal = 'start';
   protected layoutVertical = 'stretch';
 
-  dataHorizontal: Array<any> = [
-    { 'value': 'none', 'label': 'none', 'checked': false },
+  @ViewChild('title')
+  public titleInput: any;
+
+  @ViewChild('elevation')
+  public elevationInput: any;
+
+  @ViewChild('icon')
+  public iconInput: any;
+
+  public dataHorizontal: Array<any> = [
     { 'value': 'start', 'label': 'start (default)', 'checked': true },
     { 'value': 'center', 'label': 'center', 'checked': false },
     { 'value': 'end', 'label': 'end', 'checked': false },
@@ -47,30 +46,22 @@ export class ContainersComponent implements OnInit {
     { 'value': 'space-between', 'label': 'space-between', 'checked': false },
     { 'value': 'space-evenly', 'label': 'space-evenly', 'checked': false }
   ];
-  dataVertical: Array<any> = [
-    { 'value': 'none', 'label': 'none', 'checked': false },
+
+  public dataVertical: Array<any> = [
     { 'value': 'start', 'label': 'start', 'checked': false },
     { 'value': 'center', 'label': 'center', 'checked': false },
     { 'value': 'end', 'label': 'end', 'checked': false },
     { 'value': 'stretch', 'label': 'stretch (default)', 'checked': true }
   ];
-  protected files: any = {};
 
-  constructor(protected navigationService: NavigationBarService) {
-  }
+  constructor(protected navigationService: NavigationBarService) { }
 
   ngOnInit() {
-    this.files = this.retrieveFiles();
     this.navigationService.setTitle('Layout > Child alignment');
   }
 
   getLayoutAlign() {
     return this.layoutHorizontal + ' ' + this.layoutVertical;
-  }
-
-  onLayoutChange(evt) {
-    this.layout = evt.value;
-    this.files = this.retrieveFiles();
   }
 
   onLayoutHorizontalChange(evt) {
@@ -81,15 +72,18 @@ export class ContainersComponent implements OnInit {
     this.layoutVertical = evt.value;
   }
 
-  getFiles() {
-    return this.files;
+  getMainDirection() {
+    return this.layout === 'row' ? 'horizontal' : 'vertical';
   }
 
-  retrieveFiles() {
-    const html = this.layout === 'row' ? HTML_ROW_DATA : HTML_COL_DATA;
+  getSecondaryDirection() {
+    return this.layout === 'column' ? 'horizontal' : 'vertical';
+  }
+
+  getFiles() {
     return {
       'html': {
-        'data': html
+        'data': this.getHTML()
       },
       'scss': {
         'data': undefined
@@ -100,11 +94,17 @@ export class ContainersComponent implements OnInit {
     };
   }
 
-  getMainDirection() {
-    return this.layout === 'row' ? 'horizontal' : 'vertical';
+  onShowSource(exampleComp: ExampleComponent) {
+    exampleComp.html = this.getHTML();
   }
 
-  getSecondaryDirection() {
-    return this.layout === 'column' ? 'horizontal' : 'vertical';
+  getHTML() {
+    return HTML_DATA
+      .replace(new RegExp('{layout}', 'g'), 'o-' + this.layout)
+      .replace('{title}', this.titleInput.nativeElement.value)
+      .replace('{icon}', this.iconInput.nativeElement.value)
+      .replace('{layoutAlign}', this.getLayoutAlign())
+      .replace('{elevation}', this.elevationInput.nativeElement.value);
   }
+
 }
