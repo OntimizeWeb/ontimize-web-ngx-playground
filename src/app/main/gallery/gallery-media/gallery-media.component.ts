6efcea37160fc0606a439fc679a0a72ea04441d3
Patricia Martinez <patricia.martinez@imatia.com>
2019-09-23 16:39:45 +0200

    Updated gallery media example

--- src/app/main/gallery/gallery-media/gallery-media.component.ts
+++ src/app/main/gallery/gallery-media/gallery-media.component.ts
@@ -5,6 +5,67 @@ const HTML_DATA = `
 <o-gallery #demoGaleria [gallery-images]="galleryImages" [gallery-options]="galleryOptions"></o-gallery>
 `;
 
+
+const TYPESCRIPT_DATA = `
+...
+ngOnInit(): void {
+
+  this.galleryOptions = [
+    {
+      breakpoint: 1920,
+      height: "600px",
+      width: "600px",
+      image: true,
+      imageArrows: true,
+      imagePercent: 100,
+      imageSize: GalleryImageSize.Cover,
+      thumbnails: true,
+      thumbnailSize: GalleryImageSize.Cover,
+      thumbnailsColumns: 3,
+      thumbnailsRows: 1,
+      thumbnailsPercent: 25,
+      thumbnailsMargin: 10,
+      thumbnailMargin: 10,
+      preview: true,
+      previewArrows: true,
+      previewAutoPlay: false,
+      previewCloseOnClick: true,
+      previewCloseOnEsc: true,
+      previewKeyboardNavigation: true,
+      previewDownload: true,
+      previewRotate: true,
+      previewZoom: true,
+      previewDescription: false,
+      previewFullscreen: true
+    }
+  ];
+
+  this.galleryImages = [
+    {
+      small: 'https://picsum.photos/id/237/150/100.jpg',
+      medium: 'https://picsum.photos/id/237/600/450.jpg',
+      big: 'https://picsum.photos/id/237/600/450.jpg'
+    },
+    {
+      small: 'https://picsum.photos/id/1062/150/100.jpg',
+      medium: 'https://picsum.photos/id/1062/600/450.jpg',
+      big: 'https://picsum.photos/id/1062/600/450.jpg'
+    },
+    {
+      small: 'https://picsum.photos/id/1012/150/100.jpg',
+      medium: 'https://picsum.photos/id/1012/600/450.jpg',
+      big: 'https://picsum.photos/id/1012/600/450.jpg'
+    },
+    {
+      small: 'https://picsum.photos/id/1025/150/100.jpg',
+      medium: 'https://picsum.photos/id/1025/600/450.jpg',
+      big: 'https://picsum.photos/id/1025/600/450.jpg'
+    }
+  ];
+}
+...
+`;
+
 @Component({
   selector: 'gallery-media',
   templateUrl: './gallery-media.component.html',
@@ -23,7 +84,7 @@ export class GalleryMediaComponent implements OnInit {
       'data': undefined
     },
     'typescript': {
-      'data': undefined
+      'data': TYPESCRIPT_DATA
     }
   };
 
