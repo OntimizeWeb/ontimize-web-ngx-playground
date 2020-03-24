53ad344e652abef7177672d38d5ed72b03054bc6
mart972 <gonzalo.martinezfernandez.extern@bertelsmann.de>
2020-03-24 16:46:01 +0100

    Adding grid components

---
+++ src/app/main/grid/grid-basic/grid-basic.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+
+import { ExampleComponent } from '../../../shared/example/example.component';
+import { GridUtils } from '../grid.utils';
+
+@Component({
+  selector: 'grid-basic',
+  templateUrl: './grid-basic.component.html',
+  styleUrls: ['./grid-basic.component.scss']
+})
+export class GridBasicComponent {
+
+  public columns = 4;
+  public pagesize = 8;
+  public sortColumn;
+  public gutterSize = 1;
+
+  getStaticData() {
+    return GridUtils.getData('o-grid-basic');
+  }
+
+  getFiles(type) {
+    return GridUtils.getFiles(type);
+  }
+
+  onShowSource(grid?: any, exampleComp?: ExampleComponent) {
+    exampleComp.html = this.replaceHtml(GridUtils.HTML_DATA_GRID, grid);
+  }
+
+
+  public replaceHtml(html: string, grid?: any) {
+    if (grid) {
+      html = html.replace('{title}', grid.title || '')
+        .replace('{columns}', grid.cols)
+        .replace('{orderable}', grid.showSort)
+        .replace('{quickfilter}', grid.quickFilter)
+        .replace('{queryRows}', grid.queryRows)
+        .replace('{controls}', grid.controls)
+        .replace('{showPageSize}', grid.showPageSize)
+        .replace('{sortColumn}', grid.sortColumn ? grid.sortColumn : '')
+        .replace('{refreshButton}', grid.refreshButton)
+        .replace('{paginationControls}', grid.paginationControls)
+        .replace('{fixedHeader}', grid.fixedHeader)
+        .replace('{gutterSize}', grid.gutterSize);
+    }
+
+    return html;
+  }
+
+  reloadGridData(grid) {
+    setTimeout(() => {
+      grid.reloadData();
+    }, 0);
+  }
+
+}
---
+++ src/app/main/grid/grid-fixed/grid-fixed.component.ts
@@ -0,0 +1,43 @@
+import { Component, ViewChild } from '@angular/core';
+
+import { GridUtils } from '../grid.utils';
+
+@Component({
+  selector: 'grid-fixed',
+  templateUrl: './grid-fixed.component.html',
+  styleUrls: ['./grid-fixed.component.scss']
+})
+
+export class GridFixedComponent {
+
+  @ViewChild('height', { static: false })
+  gridHeight: any;
+
+  getStaticData() {
+    return GridUtils.getData('o-grid-fixed');
+  }
+
+  getFiles(type) {
+    return GridUtils.getFiles(type);
+  }
+
+  onShowSource(grid?: any, exampleComp?: any) {
+    exampleComp.html = this.replaceHtml(GridUtils.HTML_DATA_GRID_FIXED, grid);
+  }
+
+  public replaceHtml(html: string, grid?: any) {
+    if (grid) {
+      html = html.replace('{title}', grid.title || '')
+        .replace('{height}', this.gridHeight.nativeElement.value)
+        .replace('{fixedHeader}', grid.fixedHeader);
+    }
+    return html;
+  }
+
+  reloadGridData(grid) {
+    setTimeout(() => {
+      grid.reloadData();
+    }, 0);
+  }
+
+}
