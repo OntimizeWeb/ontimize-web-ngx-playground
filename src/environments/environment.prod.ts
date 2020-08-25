6a930c804dc98cd3bb14d6720286e9949d7e719f
Patricia Martinez <patricia.martinez@imatia.com>
2020-08-25 14:21:11 +0200

    Added new option menu "About"

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-  version: require('../../package.json').version
+  versions: {
+    core: '4.1.3',
+    gallery: '4.0.0',
+    theming: '4.0.2'
+  }
 };
