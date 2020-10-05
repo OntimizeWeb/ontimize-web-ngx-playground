df505d8f3d0d9b0a3906e0922f4adac3a0a5e0b5
Patricia Martinez <patricia.martinez@imatia.com>
2020-10-05 08:22:23 +0200

    Removed unnecessary comments
    (cherry picked from commit 6e9a95cdf9130d7d9ff975ea252e2b16f9651379)

--- src/app/shared/services/dummy.service.ts
+++ src/app/shared/services/dummy.service.ts
@@ -36,7 +36,6 @@ export class DummyService extends OntimizeService {
   public configureService(config: any): void {
     this._urlBase = './assets/dummy-data';
     this._sessionid = config.session ? config.session.id : -1;
-    // this._user = config.session ? config.session.user : '';
 
     if (config.entity !== undefined) {
       this.entity = config.entity;
@@ -55,8 +54,8 @@ export class DummyService extends OntimizeService {
     return undefined;
   }
 
-  public query(kv?: Object, av?: Array<string>, entity?: string,
-    sqltypes?: Object): Observable<any> {
+  public query(kv?: object, av?: Array<string>, entity?: string,
+    sqltypes?: object): Observable<any> {
     entity = (Util.isDefined(entity)) ? entity : this.entity;
 
     const url = this._urlBase + DummyService.mappings[entity];
@@ -72,30 +71,10 @@ export class DummyService extends OntimizeService {
       },
       errorCallBack: this.parseUnsuccessfulQueryResponse
     });
-
-
-    // const self = this;
-    // const dataObservable: Observable<any> = new Observable(_innerObserver => {
-    //   self.httpClient.get(url, options).subscribe((resp: any) => {
-    //     if (resp && resp.code === 3) {
-    //       self.redirectLogin(true);
-    //     } else if (resp.code === 1) {
-    //       _innerObserver.error(resp.message);
-    //     } else if (resp.code === 0) {
-    //       resp.data = self.filterResponse(kv, resp);
-    //       _innerObserver.next(resp);
-    //     } else {
-    //       // Unknow state -> error
-    //       _innerObserver.error('Service unavailable');
-    //     }
-    //   }, error => _innerObserver.error(error),
-    //     () => _innerObserver.complete());
-    // });
-    // return dataObservable.pipe(share());
   }
 
   public advancedQuery(kv?: Object, av?: Array<string>, entity?: string, sqltypes?: Object,
-    offset?: number, pagesize?: number, orderby?: Array<Object>): Observable<any> {
+      offset?: number, pagesize?: number, orderby?: Array<Object>): Observable<any> {
     return undefined;
   }
 
@@ -121,12 +100,11 @@ export class DummyService extends OntimizeService {
       resp.data = this.filterResponse(kv, resp);
       subscriber.next(resp);
     } else {
-      // Unknow state -> error
       subscriber.error('Service unavailable');
     }
   }
 
-  private filterResponse(kv: Object, resp) {
+  private filterResponse(kv: object, resp) {
 
     if (kv.hasOwnProperty(FilterExpressionUtils.FILTER_EXPRESSION_KEY)) {
       return this.fetchRoots(resp);
@@ -144,21 +122,6 @@ export class DummyService extends OntimizeService {
       }
     });
     return result;
-    // if (keys.length > 0 && resp.data && resp.data.length > 0) {
-    //   for (let i = resp.data.length - 1; i >= 0; i--) {
-    //     let deleteRecord = false;
-    //     for (let k = 0, lenk = keys.length; k < lenk; k++) {
-    //       if (resp.data[i][keys[k]] !== kv[keys[k]]) {
-    //         deleteRecord = true;
-    //         break;
-    //       }
-    //     }
-    //     if (deleteRecord) {
-    //       resp.data.splice(i, 1);
-    //     }
-    //   }
-    // }
-
   }
 
   private fetchRoots(resp: any): Array<any> {
