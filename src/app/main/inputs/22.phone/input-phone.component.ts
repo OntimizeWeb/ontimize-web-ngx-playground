dc3a46d8c3da14b79b9213dec1fa551005c13e31
Tomas Fuentes <tomasfacal@gmail.com>
2022-07-01 13:03:40 +0200

    added code

--- src/app/main/inputs/22.phone/input-phone.component.ts
+++ src/app/main/inputs/22.phone/input-phone.component.ts
@@ -3,24 +3,24 @@ import { Component } from '@angular/core';
 const TEXT_HTML_DATA = `
   <o-form editable-detail="no" show-header="no" layout-direction="column">
 
-    <o-text-input fxFlex attr="input" label="Text" [data]="getValue()"></o-text-input>
+    <o-phone-input fxFlex attr="input" label="phone" [data]="getValue()"></o-phone-input>
 
-    <o-text-input attr="input2" label="Text" [data]="getValue()" read-only="no" required="yes" ></o-text-input>
+    <o-phone-input attr="input2" label="phone" [data]="getValue()" read-only="no"required="yes"></o-phone-input>
 
-    <o-text-input attr="input3" label="Text" enabled="no" [data]="getValue()"></o-text-input>
+    <o-phone-input attr="input3" label="phone" enabled="no" [data]="getValue()"></o-phone-input>
 
   </o-form>
 `;
 
 const TEXT_TS_DATA = `
-  @Component({
-    selector: 'input-text',
-    templateUrl: 'input-text.component.html'
-  })
-  export class InputTextComponent {
+ @Component({
+  selector: 'input-phone',
+  templateUrl: 'input-phone.component.html'
+})
+  export class InputPhoneComponent {
 
     getValue() {
-      return 'John Doe';
+      return '+34 986350677';
     }
 
   }
