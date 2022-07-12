55679ff8c1c4521e37069a0277bccd062dfce34b
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2022-07-12 09:29:16 +0200

    Updated image descriptions

--- src/app/main/gallery/gallery-media/gallery-media.component.ts
+++ src/app/main/gallery/gallery-media/gallery-media.component.ts
@@ -12,54 +12,38 @@ ngOnInit(): void {
 
   this.galleryOptions = [
     {
-      breakpoint: 1920,
-      height: "600px",
-      width: "600px",
-      image: true,
-      imageArrows: true,
-      imagePercent: 100,
-      imageSize: GalleryImageSize.Cover,
-      thumbnails: true,
-      thumbnailSize: GalleryImageSize.Cover,
-      thumbnailsColumns: 3,
-      thumbnailsRows: 1,
-      thumbnailsPercent: 25,
-      thumbnailsMargin: 10,
-      thumbnailMargin: 10,
-      preview: true,
-      previewArrows: true,
-      previewAutoPlay: false,
-      previewCloseOnClick: true,
-      previewCloseOnEsc: true,
-      previewKeyboardNavigation: true,
-      previewDownload: true,
-      previewRotate: true,
-      previewZoom: true,
-      previewDescription: false,
-      previewFullscreen: true
-    }
+     breakpoint: 1720,
+        height: "600px",
+        width: "600px",
+        imagePercent: 100,
+        thumbnailsColumns: 3
+      }
   ];
 
   this.galleryImages = [
     {
       small: 'https://picsum.photos/id/237/150/100.jpg',
       medium: 'https://picsum.photos/id/237/600/450.jpg',
-      big: 'https://picsum.photos/id/237/600/450.jpg'
+      big: 'https://picsum.photos/id/237/600/450.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac nisi nunc. Donec hendrerit lorem nisi, tincidunt maximus purus facilisis ac.'
     },
     {
       small: 'https://picsum.photos/id/1062/150/100.jpg',
       medium: 'https://picsum.photos/id/1062/600/450.jpg',
-      big: 'https://picsum.photos/id/1062/600/450.jpg'
+      big: 'https://picsum.photos/id/1062/600/450.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ullamcorper rhoncus urna id pretium. Donec rhoncus nisl nulla, eu fermentum ligula rutrum ut.'
     },
     {
       small: 'https://picsum.photos/id/1012/150/100.jpg',
       medium: 'https://picsum.photos/id/1012/600/450.jpg',
-      big: 'https://picsum.photos/id/1012/600/450.jpg'
+      big: 'https://picsum.photos/id/1012/600/450.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Ut ac massa semper, facilisis turpis porta, dapibus elit.Integer vestibulum ipsum quis ultricies efficitur.'
     },
     {
       small: 'https://picsum.photos/id/1025/150/100.jpg',
       medium: 'https://picsum.photos/id/1025/600/450.jpg',
-      big: 'https://picsum.photos/id/1025/600/450.jpg'
+      big: 'https://picsum.photos/id/1025/600/450.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vestibulum ex nulla, quis imperdiet ex interdum vel. Duis sit amet placerat purus, quis sodales ante.'
     }
   ];
 }
@@ -117,25 +101,25 @@ export class GalleryMediaComponent implements OnInit {
         small: 'https://picsum.photos/id/237/150/100.jpg',
         medium: 'https://picsum.photos/id/237/600/450.jpg',
         big: 'https://picsum.photos/id/237/600/450.jpg',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac nisi nunc. Donec hendrerit lorem nisi, tincidunt maximus purus facilisis ac. Ut convallis eros vitae libero hendrerit, quis commodo felis.'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac nisi nunc. Donec hendrerit lorem nisi, tincidunt maximus purus facilisis ac.'
       },
       {
         small: 'https://picsum.photos/id/1062/150/100.jpg',
         medium: 'https://picsum.photos/id/1062/600/450.jpg',
         big: 'https://picsum.photos/id/1062/600/450.jpg',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ullamcorper rhoncus urna id pretium. Donec rhoncus nisl nulla, eu fermentum ligula rutrum ut. Pellentesque tristique consectetur mauris et facilisis. Morbi.'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ullamcorper rhoncus urna id pretium. Donec rhoncus nisl nulla, eu fermentum ligula rutrum ut. Pellentesque tristique consectetur mauris et facilisis. '
       },
       {
         small: 'https://picsum.photos/id/1012/150/100.jpg',
         medium: 'https://picsum.photos/id/1012/600/450.jpg',
         big: 'https://picsum.photos/id/1012/600/450.jpg',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Ut ac massa semper, facilisis turpis porta, dapibus elit.Integer vestibulum ipsum quis ultricies efficitur.Maecenas in lorem sed odio rutrum rhoncus.'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Ut ac massa semper, facilisis turpis porta, dapibus elit.Integer vestibulum ipsum quis ultricies efficitur.'
       },
       {
         small: 'https://picsum.photos/id/1025/150/100.jpg',
         medium: 'https://picsum.photos/id/1025/600/450.jpg',
         big: 'https://picsum.photos/id/1025/600/450.jpg',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vestibulum ex nulla, quis imperdiet ex interdum vel. Duis sit amet placerat purus, quis sodales ante. Donec congue, felis ac vehicula.'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vestibulum ex nulla, quis imperdiet ex interdum vel. Duis sit amet placerat purus, quis sodales ante.'
       }
     ];
   }
