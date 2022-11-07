82b172c26f81cb4a6fabe2486759605975cc584a
Javier Fernández Méndez <fernandezmendez99@gmail.com>
2022-11-07 16:13:18 +0100

    menu finished

--- src/app/main/inputs/inputs.component.ts
+++ src/app/main/inputs/inputs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
 import { OColumnCollapsibleComponent, OTranslateService } from 'ontimize-web-ngx';
 
 import { NavigationBarService } from '../../shared/navigation-bar.service';
@@ -11,34 +11,31 @@ import { NavigationBarService } from '../../shared/navigation-bar.service';
 })
 export class InputsComponent implements OnInit {
 
-  private expanded: boolean;
-
   @ViewChild("collapsible", {static: true}) collapsible: OColumnCollapsibleComponent;
 
   constructor(
     protected navigationService: NavigationBarService,
     protected translateService: OTranslateService
-  ) {
-    this.expanded = true;
-
-  }
+  ) { }
 
   ngOnInit() {
     let title = '';
     title += this.translateService.get('INPUTS');
     title = title + ' > ' + this.translateService.get('INPUTS');
     this.navigationService.setTitle(title);
+
   }
 
-  /*col_click() {
-    if (this.expanded) {
-      this.expanded = false;
+  status() {
+    if (this.collapsible.expPanel._getExpandedState() == "collapsed") {
       this.collapsible.title = "";
+      document.getElementById("collapsible").className = "collapsed";
     }
     else {
-      this.expanded = true;
       this.collapsible.title = this.translateService.get('SELECTION');
+      document.getElementById("collapsible").className = "o-column-collapsible";
     }
-  }*/
+  }
+
 
 }
