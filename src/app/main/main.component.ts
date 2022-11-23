cb417c4704af9f3353e5d2ddfb3bb198a2e94b78
Javier Fernández Méndez <fernandezmendez99@gmail.com>
2022-11-23 14:27:57 +0100

    dark theme not working

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,6 +2,7 @@ import { Component, Injector, OnInit, ViewEncapsulation } from '@angular/core';
 import { AuthService } from 'ontimize-web-ngx';
 
 import { NavigationBarService } from '../shared/navigation-bar.service';
+import { ThemeService } from '../shared/theme.service';
 
 @Component({
   selector: 'app-main',
@@ -12,22 +13,38 @@ import { NavigationBarService } from '../shared/navigation-bar.service';
 export class MainComponent implements OnInit {
   protected sectionTitle = '';
 
+  private light: boolean = true;
+
   constructor(
     protected injector: Injector,
-    protected navigationService: NavigationBarService
+    protected navigationService: NavigationBarService,
+    protected _themeService: ThemeService
   ) {
     const authService = this.injector.get(AuthService);
     authService.storeSessionInfo({
       id: 12345,
       user: 'playground'
     });
+
   }
 
   ngOnInit() {
     this.navigationService.onTitleChange((title) => {
       this.sectionTitle = title;
     });
+
   }
 
+  light_mode() {
+    const currentTheme = this._themeService.getStoredTheme();
+    currentTheme.isDark = false;
+    this._themeService.installTheme(currentTheme);
+  }
+
+  dark_mode() {
+    const currentTheme = this._themeService.getStoredTheme();
+    currentTheme.isDark = true;
+    this._themeService.installTheme(currentTheme);
+  }
 
 }
