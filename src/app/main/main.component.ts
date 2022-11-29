1fc1eb8a31a8186aa0307c0a6c425c60f5f7a7f0
Javier Fernández Méndez <fernandezmendez99@gmail.com>
2022-11-29 13:30:45 +0100

    css generated

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -13,7 +13,7 @@ import { ThemeService } from '../shared/theme.service';
 export class MainComponent implements OnInit {
   protected sectionTitle = '';
 
-  private light: boolean = true;
+  private light: boolean;
 
   constructor(
     protected injector: Injector,
@@ -32,19 +32,27 @@ export class MainComponent implements OnInit {
     this.navigationService.onTitleChange((title) => {
       this.sectionTitle = title;
     });
-
+    const currentTheme = this._themeService.getStoredTheme();
+    if (currentTheme.isDark) {
+      this.light = false;
+    }
+    else {
+      this.light = true;
+    }
   }
 
   light_mode() {
     const currentTheme = this._themeService.getStoredTheme();
     currentTheme.isDark = false;
     this._themeService.installTheme(currentTheme);
+    this.light = true;
   }
 
   dark_mode() {
     const currentTheme = this._themeService.getStoredTheme();
     currentTheme.isDark = true;
     this._themeService.installTheme(currentTheme);
+    this.light = false;
   }
 
 }
