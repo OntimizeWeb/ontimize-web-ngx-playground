29ba6624f6183067011692bfb626ca499fea53e9
Javier Fernández Méndez <fernandezmendez99@gmail.com>
2022-12-02 10:41:23 +0100

    updated ontimize verison

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   versions: {
-    core: '8.7.0',
+    core: '8.8.0',
     gallery: '8.1.0',
     theming: '8.3.0'
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   versions: {
-    core: '8.7.0',
+    core: '8.8.0',
     gallery: '8.1.0',
     theming: '8.3.0'
   }
