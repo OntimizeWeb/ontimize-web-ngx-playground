cfe7f69367c1310c49e2784beff9fd2105bd40e7
Javier Fernández Méndez <fernandezmendez99@gmail.com>
2022-12-05 15:08:20 +0100

    dark mode inputs card menu added

--- src/app/shared/inputs-card/inputs-card.component.ts
+++ src/app/shared/inputs-card/inputs-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ThemeService } from '../theme.service';
 
 @Component({
   selector: 'inputs-card',
@@ -13,5 +14,20 @@ export class InputsCardComponent {
   public content: string;
   public compName: string;
   public new: boolean = false;
+  public dark: boolean;
+
+  constructor(protected _themeService: ThemeService) {
+    _themeService.onThemeUpdate.subscribe(() => {
+      const currentTheme = this._themeService.getStoredTheme();
+      currentTheme.isDark ? this.dark = true : this.dark = false;
+    });
+  }
+
+  ngOnInit(): void {
+    const currentTheme = this._themeService.getStoredTheme();
+    currentTheme.isDark ? this.dark = true : this.dark = false;
+  }
+
+
 
 }
