a793c83031b5e8a79519a62ee84447b1091235c3
Javier Fernández Méndez <fernandezmendez99@gmail.com>
2023-03-08 15:51:08 +0100

    new home screen

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OTranslateService } from 'ontimize-web-ngx';
+import { Subscription } from 'rxjs';
 import { NavigationBarService } from '../../shared/navigation-bar.service';
+import { DocsSiteTheme, ThemeService } from '../../shared/theme.service';
 
 @Component({
   selector: 'home',
@@ -9,14 +11,25 @@ import { NavigationBarService } from '../../shared/navigation-bar.service';
 })
 export class HomeComponent implements OnInit {
 
+  is_dark_theme: boolean;
+  private onThemeUpdate: Subscription;
+
   constructor(
     protected navigationService: NavigationBarService,
-    protected translateService: OTranslateService) {
+    protected translateService: OTranslateService,
+    protected themeService: ThemeService) {
   }
 
   ngOnInit() {
     let title = '';
     title = this.translateService.get('INTRODUCTION');
     this.navigationService.setTitle(title);
+    this.onThemeUpdate = this.themeService.onThemeUpdate.subscribe((theme: DocsSiteTheme) => {
+      this.is_dark_theme = theme.isDark;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.onThemeUpdate.unsubscribe();
   }
 }
