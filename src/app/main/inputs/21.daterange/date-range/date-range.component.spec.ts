31db9a670b890fdbf2582955604d3a6226ff6870
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2023-05-08 14:45:44 +0200

    Migration to Angular 10

--- src/app/main/inputs/21.daterange/date-range/date-range.component.spec.ts
+++ src/app/main/inputs/21.daterange/date-range/date-range.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { InputDateRangeComponent } from './date-range.component';
 
 
@@ -6,7 +6,7 @@ describe('InputDateRangeComponent', () => {
   let component: InputDateRangeComponent;
   let fixture: ComponentFixture<InputDateRangeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ InputDateRangeComponent ]
     })
