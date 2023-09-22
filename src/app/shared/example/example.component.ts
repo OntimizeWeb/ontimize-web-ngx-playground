2ec86735e2162abc0d3e2e433ff6b83a11206165
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2023-09-22 09:04:42 +0200

    Replaced InputConverter by BooleanInputConverter

--- src/app/shared/example/example.component.ts
+++ src/app/shared/example/example.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, ViewEncapsulation, ChangeDetectorRef } from '@angular/core';
-import { InputConverter } from 'ontimize-web-ngx';
+import { BooleanInputConverter } from 'ontimize-web-ngx';
 
 export interface IFiles {
   html?: any;
@@ -38,10 +38,10 @@ export class ExampleComponent {
   _files: IFiles = {};
   tabHeight: string = '350px';
 
-  @InputConverter()
+  @BooleanInputConverter()
   collapsible: boolean = false;
 
-  @InputConverter()
+  @BooleanInputConverter()
   collapsed: boolean = false;
 
   onShowSource: EventEmitter<any> = new EventEmitter<any>();
