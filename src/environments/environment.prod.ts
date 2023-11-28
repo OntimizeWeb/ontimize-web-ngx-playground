81058e50479ec3fe29d4ec563988aa0b15a6024b
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2023-11-28 16:37:09 +0100

    Updated ontimize-web-ngx versions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   versions: {
-    core: '15.0.0-beta.1',
-    gallery: '15.0.0-beta.0'
+    core: '15.0.0-beta.4',
+    gallery: '15.0.0'
   }
 };
