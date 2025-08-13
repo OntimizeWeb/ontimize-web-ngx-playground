73156e5ca558a4504500d01855d7b97ba679b859
Patricia Martinez Tilve <patricia.martinez@imatia.com>
2025-08-13 12:26:01 +0200

    Updated app.services.config.ts

--- src/app/shared/app.services.config.ts
+++ src/app/shared/app.services.config.ts
@@ -1,5 +1,25 @@
 export const SERVICE_CONFIG = {
   'customers': {
     'path': '/customers'
+  },
+  'vehicles': {
+    'path': '/vehicles',
+    'urlBase': 'https://swapi.info/api'
+  },
+  'users': {
+    'path': '',
+    'urlBase': 'https://randomuser.me/api'
+  },
+  'characters': {
+    'urlBase': 'https://rickandmortyapi.com/api',
+    'path': '/character'
+  },
+  "locations": {
+    'urlBase': 'https://rickandmortyapi.com/api',
+    'path': '/location'
+  },
+  "episodes": {
+    'urlBase': "https://rickandmortyapi.com/api",
+    'path': 'episode'
   }
 };
