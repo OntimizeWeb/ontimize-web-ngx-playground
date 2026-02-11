28b678e11ebf60099f6c7ed0c49fdeffcec93c15
Noelia Prol Carballo <noelia.prol@imatia.com>
2026-02-11 10:42:41 +0100

    fix: tree icon

--- src/app/shared/app.menu.config.ts
+++ src/app/shared/app.menu.config.ts
@@ -15,7 +15,7 @@ export const MENU_CONFIG: MenuRootItem[] = [
       { id: 'list', name: 'LIST.COMPONENT', route: '/main/data/lists', icon: 'list' },
       { id: 'table', name: 'TABLE.COMPONENT', route: '/main/data/table', icon: 'storage' },
       { id: 'tree', name: 'TREE.COMPONENT', route: '/main/data/tree', icon: 'account_tree' },
-      { id: 'data-view', name: 'DATA-VIEW.COMPONENT', route: '/main/data/data-view', icon: 'swap_horiz'}
+      { id: 'data-view', name: 'DATA-VIEW.COMPONENT', route: '/main/data/data-view', icon: 'view_quilt'}
     ]
   },
   {
