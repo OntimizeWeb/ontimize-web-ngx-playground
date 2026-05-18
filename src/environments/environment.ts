d4a6256b3ffe0d32453beca774ffd39a60cc4834
Patricia Martinez Tilve <patricia.martinez@imatia.com>
2026-05-18 10:44:09 +0200

    chore: Bumping latest ontimize-web-ngx version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,9 @@
 export const environment = {
   production: false,
   versions: {
-    core: '15.9.0',
-    extraComponentes:'15.0.0',
-    gallery: '15.1.2'
+    core: '18.0.0-next.3',
+    extraComponentes:'18.0.0-next.1',
+    gallery: '18.0.0-next.0'
   }
 };
 
