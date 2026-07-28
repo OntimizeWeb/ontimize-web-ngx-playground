c04dacf6f31642cb019f782f4eec0f4746e89d1e
Patricia Martinez Tilve <patricia.martinez@imatia.com>
2026-07-28 11:50:25 +0200

    Fix (karma): restore karma-sonarqube-unit-reporter dependency and fix app.component.spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,6 @@
-import { AppModule } from './app.module';
-import { Injector } from '@angular/core';
 import { TestBed, ComponentFixture, waitForAsync } from '@angular/core/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppConfig, AppConfigFactory, APP_CONFIG, OntimizeWebModule, ONTIMIZE_PROVIDERS, OPermissionsModule } from 'ontimize-web-ngx';
+import { provideOntimizeWeb } from 'ontimize-web-ngx';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -14,21 +11,14 @@ describe('AppComponent', () => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        OntimizeWebModule,
-        OPermissionsModule,
-        NoopAnimationsModule,
-        AppModule
+        AppComponent
       ],
       providers: [
-        {
-          provide: APP_CONFIG, useValue: {
-            uuid: 'com.ontimize.web.test',
-            title: 'Ontimize Web Testing',
-            locale: 'en'
-          }
-        },
-        { provide: AppConfig, useFactory: AppConfigFactory, deps: [Injector] },
-        ...ONTIMIZE_PROVIDERS
+        provideOntimizeWeb({
+          uuid: 'com.ontimize.web.test',
+          title: 'Ontimize Web Testing',
+          locale: 'en'
+        }, { noAnimations: true })
       ]
     }).compileComponents();
   }));
